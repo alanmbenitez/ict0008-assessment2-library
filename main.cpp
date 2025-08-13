3096f3159c748dbafea014ae443a4f770c61ce3e
Alan Benitez <alanmbenitezz@gmail.com>
2025-08-13 16:31:06 +1000

    feat: add Library with sort/search/borrow

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 
-
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -10,7 +9,7 @@ struct Book {
     std::string author;
     std::string isbn;
     bool available;
-    std::string dateAdded; 
+    std::string dateAdded; // simple text to keep it easy
 
     // Assign values to attributes
     void setBookDetails() {
@@ -19,17 +18,35 @@ struct Book {
 
     // Show the book information
     void displayBookDetails() const {
-        
+       
     }
 
     // Borrow the book (only if available)
     bool borrowBook() {
-       
+        
     }
 
     // Return the book
     void returnBook() {
-      
+       
     }
 };
 
+struct Library {
+    Book books[5];
+
+    // Sort array by ISBN (ascending)
+    void sortBookData() {
+        
+    }
+
+    // Find book index by ISBN, return -1 if not found
+    int findByIsbn(const std::string& id) {
+       
+    }
+
+    // Try to borrow by ISBN
+    bool borrowByIsbn(const std::string& id) {
+       
+    }
+};
